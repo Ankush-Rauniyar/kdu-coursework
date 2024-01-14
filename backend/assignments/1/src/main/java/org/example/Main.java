b1b2b4e0098726c59caa452d8e24f04f9be145ad
Ankush-Rauniyar <ankush.rauniyar@kickdrumtech.com>
2024-01-15 00:16:40 +0530

    checking update

--- backend/assignments/1/src/main/java/org/example/Main.java
+++ backend/assignments/1/src/main/java/org/example/Main.java
@@ -14,6 +14,7 @@ import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class Main {
+
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void executeTransactions(JsonNode jsonTransactions, CountDownLatch latch){
         ExecuteTransaction.countDownLatch = latch;
